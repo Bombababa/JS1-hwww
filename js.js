195ce6b2dce0c0391fc15fed5f4734d654c22a1b
Bombababa <136107476+Bombababa@users.noreply.github.com>
2024-07-24 20:01:07 +0300

    Create js.js
    да

---
+++ js.js
@@ -0,0 +1,39 @@
+   
+        let age = 15; 
+        console.log(age);
+
+
+        let name = "Емільєн"; 
+        console.log(name);
+
+       let isStudent = true;
+        console.log(isStudent);
+
+
+        let myString = "Кто мы? Мы путь в будущее";
+        console.log(myString);
+
+      let myNumber = 20; 
+        myNumber += 10;
+        console.log(myNumber);
+
+       let myNull = null;
+        console.log(myNull);
+
+        let userName = prompt("Будь ласка, введіть ваше ім'я:");
+        alert("Привіт, " + userName + "!");
+
+         let isConfirmed = confirm("Ви впевнені?");
+        if (isConfirmed) {
+            alert("Дякую за підтвердження!");
+        } else {
+            alert("Дію відмінено!");
+        }
+
+         alert("Увага! Ця дія може бути небезпечною.");
+        let isDangerConfirmed = confirm("Ви впевнені, що хочете продовжити?");
+        if (isDangerConfirmed) {
+            alert("Дякую за підтвердження!");
+        } else {
+            alert("Дію відмінено!");
+        }
